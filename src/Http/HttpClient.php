76462c4b4b846a11c08146201a08bfdc25f29483
Petr Král <kral.petr.88@gmail.com>
2021-05-31 11:07:51 +0200

    Github - update actions

--- src/Http/HttpClient.php
+++ src/Http/HttpClient.php
@@ -6,8 +6,8 @@ namespace EcomailFlexibee\Http;
 
 use EcomailFlexibee\Config;
 use EcomailFlexibee\Http\Response\FlexibeeResponse;
+use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
-use League\Flysystem\Local\LocalFilesystemAdapter;
 use function basename;
 use function curl_errno;
 use function curl_error;
@@ -23,6 +23,7 @@ use function number_format;
 use function sprintf;
 use function trim;
 use const CURLINFO_HTTP_CODE;
+use const FILE_APPEND;
 
 final class HttpClient
 {
@@ -86,7 +87,7 @@ final class HttpClient
             }
 
             $rootDir = dirname($config->getLogFilePath());
-            $fileSystem = new Filesystem(new LocalFilesystemAdapter($rootDir,null));
+            $fileSystem = new Filesystem(new Local($rootDir, FILE_APPEND));
             $logContent = sprintf(
                 '%s METHOD: %s URL:%s TIME:%s STATUS:%s HEADERS: %s',
                 date('Y-m-d H:i:s'),
@@ -102,7 +103,7 @@ final class HttpClient
             }
 
             $logContent .= "\n";
-            $fileSystem->write(
+            $fileSystem->put(
                 basename($config->getLogFilePath()),
                 $logContent,
             );
